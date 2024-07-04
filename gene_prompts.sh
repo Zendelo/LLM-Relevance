bc267efbe90c479775e2a7fee8dd07a1d611d838
Zendelo <s3806763@student.rmit.edu.au>
2024-07-04 11:40:32 +1000

    update bash scripts

--- gene_prompts.sh
+++ gene_prompts.sh
@@ -1,10 +1,10 @@
 # Generate prompts for the LLM4Eval dataset
 # iterate over the different prompts and generate judgments
 
-for i in {7..1}
+for i in {9..5}
 do
     python construct_prompts.py system_message_dict.json \
     -pk $i -q data/llm4eval_query_2024.txt \
-    -qr val_set_qrel.tsv   \
-    --output val_rel_prompt-$i.tsv
+    -qr train_set_qrel.tsv   \
+    --output train_rel_prompt-$i.tsv
 done
