df5ac89cf0220d5ca911a0dc15c51bd8fe54ef4c
Zendelo <s3806763@student.rmit.edu.au>
2024-07-08 10:58:31 +1000

    add endure dir to utils

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
-import random
-import torch
 import os
+import random
+
 import numpy as np
+import torch
+
+TREC_RES_COLUMNS = ['qid', 'iteration', 'docNo', 'rank', 'docScore', 'method']
+TREC_QREL_COLUMNS = ['qid', 'iteration', 'docNo', 'rel']
+
 
 def replicability(seed=None):
     np.random.seed(seed)
@@ -13,17 +18,30 @@ def replicability(seed=None):
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
-    #torch.use_deterministic_algorithms(True)
+    # torch.use_deterministic_algorithms(True)
     torch.use_deterministic_algorithms(True, warn_only=True)
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 
 
-
-
-
-
-
-
-
-
-
+def ensure_dir(file_path, create_if_not=True):
+    """
+    The function ensures the dir exists,
+    if it doesn't it creates it and returns the path or raises FileNotFoundError
+    In case file_path is an existing file, returns the path of the parent directory
+    """
+    # tilde expansion
+    file_path = os.path.normpath(os.path.expanduser(file_path))
+    if os.path.isfile(file_path):
+        directory = os.path.dirname(file_path)
+    else:
+        directory = file_path
+    if not os.path.exists(directory):
+        if create_if_not:
+            try:
+                os.makedirs(directory)
+            except FileExistsError:
+                # This exception was added for multiprocessing, in case multiple process try to create the directory
+                pass
+        else:
+            raise FileNotFoundError(f"The directory {directory} doesnt exist, create it or pass create_if_not=True")
+    return directory
